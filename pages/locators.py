553bfa388e33ffd625f3486e19ec82779490f6d8
Volodymyr Romaniv <vlmromaniv@gmail.com>
2024-03-14 15:10:58 +0200

    Add product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,17 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+
+    ADD_SUCCESS_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[1]')
+    TOTAL_COST_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[3]')
+    ITEM_NAME_ADD_SUCCESS_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[1]//strong')
+    TOTAL_COST_TOTAL_COST_MESSAGE = (By.XPATH, '//*[@id="messages"]/div[3]//strong')
+
+    ITEM_NAME_PRODUCT_MAIN = (By.CSS_SELECTOR, '.col-sm-6.product_main h1')
+    ITEM_PRICE_PRODUCT_MAIN = (By.CSS_SELECTOR, 'p.price_color ')
+    
+
