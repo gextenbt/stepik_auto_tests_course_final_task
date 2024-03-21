a8b1c23b1f6758f18b11dc07620febf163441f4c
Volodymyr Romaniv <vlmromaniv@gmail.com>
2024-03-21 20:06:06 +0200

    Add config.py with product links

---
+++ config.py
@@ -0,0 +1,11 @@
+BASE_URL = "http://selenium1py.pythonanywhere.com/"
+HOME_PAGE_PATH = ""
+PRODUCT_PAGE_PATH = "catalogue/"
+
+
+class MainPageUrls:
+    MAIN = f"{BASE_URL}{HOME_PAGE_PATH}"
+
+
+class ProductPageUrls:
+    CATALOGUE = f"{BASE_URL}{PRODUCT_PAGE_PATH}"
