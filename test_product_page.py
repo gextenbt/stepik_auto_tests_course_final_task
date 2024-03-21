022cb158cf0e4fe1c8256e0c6924d075ba3e0634
Volodymyr Romaniv <vlmromaniv@gmail.com>
2024-03-21 20:09:07 +0200

    test: Add parametrization add to basket test
    Change add to basket test:
    * add parametrization
    * mark xfail tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,24 @@
-from .pages.product_page import ProductPage
+from pages.product_page import ProductPage
+from config import ProductPageUrls
 from time import sleep
+import pytest 
+from utils import mark_param_list
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+offer_promo_params = [i for i in range(2)]
+xfail_offer_marks = [7]
+marked_offer_params = mark_param_list(
+    params=offer_promo_params,
+    mark_xfail=xfail_offer_marks
+    )
+
+
+@pytest.mark.parametrize(
+    'value', marked_offer_params
+    )
+def test_guest_can_add_product_to_basket_on_promo_offers(browser, value):
+    link = (f"{ProductPageUrls.CATALOGUE}"
+            f"coders-at-work_207/?promo=offer{value}")
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket()
